4ce5ec99326c7ed4a2c58b2e4be597c625c79d09
BerTepesh <Ber.Tepesh@gmail.com>
2016-12-29 16:17:10 +0530

    +

--- src/f/js/main.js
+++ src/f/js/main.js
@@ -1,6 +1,7 @@
 //= partials/jquery-1.11.3.min.js
 //= partials/jquery.flexslider-min.js
 //= partials/jquery.fancybox.pack.js
+//= partials/stickyfill.min.js
 //-----------------
 //----- Custom ----
 //-----------------
